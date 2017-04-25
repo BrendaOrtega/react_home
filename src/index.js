f1cccf50dcc2a3646816c66b0b5eb0f89c0f4adb
BlisS <contacto@fixter.org>
2017-04-24 21:34:50 -0500

    93/100

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Router, browserHistory} from 'react-router';
 import routes from "./routes";
 // import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css?global';
+import '../node_modules/font-awesome/css/font-awesome.css?global';
 import './app.css';
 
 
