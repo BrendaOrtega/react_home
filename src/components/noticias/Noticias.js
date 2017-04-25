3433564f6eef2b04ddc156e9ab11d44d55ea4a9a
Brenda Gonzalez <bremin11.20.93@gmail.com>
2017-04-24 19:05:08 -0500

    triste

--- src/components/noticias/Noticias.js
+++ src/components/noticias/Noticias.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {Link} from "react-router";
 import css from './Noticias.css';
+import milenio from '../../assets/mil.png';
+import gob from '../../assets/gobmx.png';
+import hidalgo from '../../assets/hidalgo.png'
+import inde from '../../assets/indep.png';
 
 class Noticias extends React.Component {
     render(){
@@ -11,22 +15,22 @@ class Noticias extends React.Component {
                 <div className="row">
                     <div className={' col-lg-3 col-md-3 col-sm-3 col-xs-6 bot '}>
                         <Link href="http://www.milenio.com/firmas/columna_ciencia_y_tecnologia_columna_ciencia_y_tecnologia/nerd_18_759104125.html" target="_blank">
-                        <img className={css.image} src="https://jorgecastaneda.org/wp-content/uploads/2016/08/milenio_2016.png" alt="Milenio"/>
+                        <img className={css.image} src={milenio} alt="Milenio"/>
                         </Link>
                     </div>
                     <div className={' col-lg-3 col-md-3 col-sm-3 col-xs-6 bot '}>
                         <Link href="http://www.gob.mx/se/prensa/otorgan-premio-a-los-ganadores-del-reto-crowfunding-reto-zapopan"  target="_blank">
-                        <img className={css.image} src="https://consulmex.sre.gob.mx/filadelfia/images/CMF/Enterate/2017/gobmx01.jpg" alt="Gob.mx"/>
+                        <img className={css.image} src={gob} alt="Gob.mx"/>
                         </Link>
                     </div>
                     <div className={' col-lg-3 col-md-3 col-sm-3 col-xs-6 bot '}>
                     <Link href="http://www.elindependientedehidalgo.com.mx/archivo/2016/04/324454" target="_blank">
-                        <img className={css.imagen} src="http://misdestacados.com/images/destacados/el_independiente_de_hidalgo.png" alt="Independiente"  />
+                        <img className={css.imagen} src={inde} alt="Independiente"  />
                     </Link>
                 </div>
                     <div className={' col-lg-3 col-md-3 col-sm-3 col-xs-6 bot '}>
                         <Link href="http://comunicacion.hidalgo.gob.mx/?p=26428" target="_blank">
-                        <img className={css.imagecenter} src="http://www.crystalsoft.com.mx/wp-content/uploads/2015/09/hidalgo.png" alt="Hidalgo"  />
+                        <img className={css.imagecenter} src={hidalgo} alt="Hidalgo"  />
                         </Link>
                     </div>
 
